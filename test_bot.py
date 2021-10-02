dcaf8621dd31ecd8b87c63fb96033ce27d51f17b
ekeminij <82672210+ekeminij@users.noreply.github.com>
2021-10-01 21:46:50 -0400

    Created framework

---
+++ test_bot.py
@@ -0,0 +1,24 @@
+# test_bot.py
+
+import discord
+from discord_slash import SlashCommand # Importing the newly installed library.
+from discord.ext import commands
+
+TOKEN = "ODkzNjc1MTk5MTkzNTY3MjUy.YVe54w._czA0yA1yrfFBWP0TrqUMsCfLAU"
+
+intents = discord.Intents.all()
+intents.members = True  # Subscribe to the privileged members intent.
+bot_var = commands.Bot(command_prefix='$',case_insensitive=True, intents=intents, help_command=None)
+slash_var = SlashCommand(bot_var, sync_commands=True, sync_on_cog_reload = True)
+
+cogs = []
+
+for cog in cogs:
+    print(cog)
+    bot_var.load_extension(cog)
+
+@bot_var.event
+async def on_ready():
+    print("Ready!")
+
+bot_var.run(TOKEN)
